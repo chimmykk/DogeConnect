938a07590492ca633aeea20ba382ee21868f2a3c
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-08-21 17:31:01 +0530

    update

---
+++ pages/api/twitter/search.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
+  const { query } = body;
+
+  // Set up your Twitter API credentials
+  const apiKey = 'YOUR_API_KEY';
+  const bearerToken = 'AAAAAAAAAAAAAAAAAAAAACzGoQEAAAAAiGtfMqCy8fNuU9Adb79MXxvc41U%3DO4abfdDIgEmMNmbmgVwL1d78ZYYwN3u2cPrDxwVggwhLBviOf7';
+
+  // Set the endpoint URL
+  const endpointUrl = 'https://api.twitter.com/2/users/by';
+
+  // Set the headers with the necessary authentication
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${bearerToken}`
+  };
+
+  // Set the request parameters
+  const params = {
+    usernames: query,
+    'user.fields': 'name,username,description,profile_image_url,public_metrics'
+  };
+
+  try {
+    // Make the API request
+    const response = await axios.get(endpointUrl, { headers, params });
+
+    // Extract the user data from the response
+    const users = response.data.data.map((user) => {
+      return {
+        username: user.username,
+        Name: user.name,
+        bio: user.description,
+        profileImageUrl: user.profile_image_url.replace('_normal', '_400x400'),
+      };
+    });
+
+    console.log(users)
+
+    res.status(200).json({ users });
+  } catch (error) {
+    if (error.response && error.response.data && error.response.data.errors) {
+      console.error('Error:', error.response.data.errors);
+      res.status(400).json({ error: error.response.data.errors });
+    } else {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  }
+};
